6f1f776ffd240e9c6d60958806a28dd7319c424c
gadaboma <gadaboma@gmail.com>
2026-02-27 09:54:53 +0300

    chore: Remove the entire onboarding feature and clean up comments in `pubspec.yaml`.

--- lib/features/onboarding/presentation/provider/on_boarding_provider.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'on_boarding_notifier.dart';
-import 'on_boarding_state.dart';
-
-final onboardingProvider = StateNotifierProvider<OnboardingNotifier, OnboardingState>((ref) {
-  return OnboardingNotifier();
-});
